05f2254be549dc9112001cc4b678c8251775f4d7
Tsenguun <Tsenguun.batbold12@gmail.com>
2022-09-07 21:21:32 +0200

    profile page config

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,114 @@
 import { useEffect, useState } from "react";
-import { accessToken, logout } from "./spotify";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
+import styled from "styled-components/macro";
+import { GlobalStyle } from "./styles";
+import { Login, Profile } from "./pages";
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(-spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 760px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+const StyledLoginContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledLoginButton = styled.a`
+  display: inline-block;
+  background-color: var(--green);
+  color: var(--white);
+  border-radius: var(--border-radius-pill);
+  font-weight: 700;
+  font-size: var(--fz-lg);
+  padding: var(--spacing-sm) var(--spacing-xl);
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    filter: brightness(1.1);
+  }
+`;
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
   }, []);
+
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
-            Login in to Spotify
-          </a>
+          <StyledLoginContainer>
+            <StyledLoginButton href="http://localhost:8888/login">
+              Log in to Spotify
+            </StyledLoginButton>
+          </StyledLoginContainer>
         ) : (
           <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log out</button>
+            <StyledLogoutButton onClick={logout}>Log out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Switch>
+                <Route path="/top-artists">
+                  <h1>Top Artist</h1>
+                </Route>
+                <Route path="/top-tracks">
+                  <h1>Top Tracks</h1>
+                </Route>
+                <Route path="/playlists/:id">
+                  <h1>Playlist</h1>
+                </Route>
+                <Route path="/playlists">
+                  <h1>Playlists</h1>
+                </Route>
+                <Route path="/">
+                  <Profile />
+                </Route>
+              </Switch>
+            </Router>
           </>
         )}
       </header>
